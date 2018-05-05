09c9a8f47fc45b09fccd9e0c457f3a4d49101fc5
Will Britton <wjbritton1@gmail.com>
2018-05-05 17:41:37 +0000

    commiting find app from CS50

---
+++ find/search.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+bool search(int value, int values[], int n);
+{
+    // TODO: implement a searching algorithm
+    int arg1 = value;
+    int arg2 = value[];
+    int arg3 = n;
+    printf("%i %i %i\n", arg1, arg2, arg3);
+}
